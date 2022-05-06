1a4b30ebe406edcae60770609adda2a61dc5048b
Ranjitrajak <102968204+Ranjitrajak@users.noreply.github.com>
2022-05-06 18:03:42 +0530

    up

--- src/controllers/interncontroller.js
+++ src/controllers/interncontroller.js
@@ -15,35 +15,35 @@ const createIntern = async function (req, res){
 
     if (!intern.mobile) { return res.status(400).send({ status: false, message: "mobile no. is required" }) }
 
-    if (!intern.collegeId) { return res.status(400).send({ status: false, message: " collegeId is required" }) }
+    if (!intern.collegeName) { return res.status(400).send({ status: false, message: " collegename is required" }) }
     if (!valid(intern.name)) { return res.status(400).send({ status: false, message: "name is invalid" }) }
+    if (!valid(intern.collegeName)) { return res.status(400).send({ status: false, message: "collegename is invalid" }) }
     let pattern = /^[A-Za-z0-9._]{3,}@[A-Za-z]{3,}[.]{1}[A-Za-z.]{2,6}$/
 
     if (!pattern.test(intern.email)) { return res.status(400).send({ status: false, message: "email is not valid" }) }
     const emailId= await Internmodel.find({email:intern.email});
     if(emailId.length!=0) return res.status(401).send({status:false,msg:"Email is Already Exist"});
     
-    let Id = intern.collegeId
-
-    let patter = /^[0-9A-Fa-f]{24}$/
-
-    if (!patter.test(Id)) { return res.status(400).send({ status: false, message: "collegeId is not valid" }) }
-        const idcheck= await collegemodel.findById(Id)
-        if(!idcheck){
-            return res.status(404).send({status:false, message:`College ID dont exist!!`})
-
-        }
+  
+       
     let check=/^(\+\d{1,3}[- ]?)?\d{10}$/
 
         if(!check.test(intern.mobile)){ return res.status(400).send({ status: false, message: "mobile number is not valid" }) }
         const mobilenumber=await Internmodel.find({mobile:intern.mobile});
     if(mobilenumber!=0) return res.status(401).send({status:false,msg:"Phone is Already Exist"})
         if (intern.isDeleted) { if (typeof intern.isDeleted !== "boolean") { return res.status(400).send({ status: false, message: "value must be in boolean" }) } }
+          let collegeFind = await collegemodel.findOne({ name:intern.collegeName },{id:1})
+
+        if (!collegeFind) {
+          return res.status(400).send({ status: false, msg: "College Name is invalid" })
+        }
 
     
 
    
 let internCreated = await Internmodel.create(intern)
+internCreated.collegeId=collegeFind._id.toString()
+internCreated.save()
 res.status(201).send({ status:true,data: internCreated })
 }
 catch (error){
